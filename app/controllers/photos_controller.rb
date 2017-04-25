cbcbaa207c972e0ea940d781e3be82f3c4daeb0d
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-04-25 11:35:00 -0500

    End of Lecture 5

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,16 +3,48 @@ class PhotosController < ApplicationController
   def show
     #the params hash looks like this: {"the_id_number"=>"1"}
     id_number = params["the_id_number"]
-    p = Photo.find(id_number)
-    @the_caption = p.caption
-    @the_image_url = p.source
-    @created_at = p.created_at
+    @photo = Photo.find(id_number)
+  #  @the_caption = p.caption
+  #  @the_image_url = p.source
+  #  @created_at = p.created_at
     #find method if given a string will convert to integer because find's job is to use params
     render("photos/show.html.erb")
   end
 
   def index
-    @all_photos = Photo.all #this gets the whole table as an array
+    #@all_photos = Photo.all #this gets the whole table as an array
+    @all_photos = Photo.all.order({:created_at => :desc })
     render("photos/index.html.erb")
+  end
+
+  def new_form
+    render("photos/new_form.html.erb")
+  end
+
+  def create_row
+    #render("photos/create_row.html.erb")
+    #the params look like this: {"the_image_url"=>"", "the_caption"=>""}
+    p = Photo.new
+    p.source = params["the_image_url"]
+    p.caption = params["the_caption"]
+    p.save
+
+    redirect_to("http://localhost:3000/photos") #or redirect_to("/photos")
+  end
+
+  def edit_form
+    @the_photo_to_edit = Photo.find(params[:the_id])
+
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    #from the server log, the params lookl like:   Parameters: {"the_source"=>"http://cdn.thedailybeast.com/content/dailybeast/articles/2017/04/03/the-internet-s-most-famous-pregnant-giraffe-still-hasn-t-given-birth/jcr:content/image.crop.800.500.jpg/49708403.cached.jpg", "the_caption"=>"giraffe", "the_id"=>"19"}
+    p = Photo.find(params[:the_id])
+    p.source = params["the_source"] #determined by the name of the input in the form
+    p.caption = params["the_caption"]
+    p.save
+
+    redirect_to("/photos")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
 
-  get("/photos/:the_id_number", {:controller => "photos", :action => "show"})
+  #create
+  get("/photos/new", {:controller => "photos", :action => "new_form"})
+  #must be above the id_number route (routes works top down)
+  get("/create_photo", {:controller => "photos", :action => "create_row"})
 
+  #READ
+  get("/photos/:the_id_number", {:controller => "photos", :action => "show"})
   get("/photos", {:controller => "photos", :action => "index"})
   #the fact that the URL is /photos is all up to us, and just a coincidence that it matches up wtih render view
+
+  #UPDATE
+  get("/photos/:the_id/edit", {:controller => "photos", :action => "edit_form"})
+get("/update_photo/:the_id", {:controller => "photos", :action => "update_row"})
+
+
+  #DELETE
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
